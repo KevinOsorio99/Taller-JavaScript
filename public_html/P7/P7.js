6c4df44d5ad5ec41db18c3154ba0e18261c2188c
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-07-22 20:34:42 -0500

    Terminado

---
+++ public_html/P7/P7.js
@@ -0,0 +1,20 @@
+var Number1=(prompt("Enter the first number:"));
+var Number2=(prompt("Enter second number:"));
+
+var result=(Number1^Number2);
+console.log(Number1 + (" To ") + Number2 + (" is ") + result);
+
+var result=(Number2^Number1);
+console.log(Number2 + (" To ") + Number1 + (" is ") + result);
+
+var result=(Number1*Number2);
+console.log("Multiplication between " + Number1 + (" and ") + Number2 + (" is ") + result);
+
+var result=(Number1/Number2);
+console.log("The division between " + Number1 + (" and ") + Number2 + (" is ") + result);
+
+var result=(Number1/Number2);
+console.log("The division between " + Number1 + (" and ") + Number2 + (" is ") + result);
+
+var result=(Number1%Number2);
+console.log("The division module between " + Number1 + (" and ") + Number2 + (" is ") + result);
