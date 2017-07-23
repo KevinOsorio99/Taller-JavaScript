677e385805c35e509dec2ed5702bc27357f9d0d8
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-07-22 20:34:29 -0500

    Terminado

---
+++ public_html/P5/P5.js
@@ -0,0 +1,8 @@
+var name= (prompt("Hi, my mombre is: "));
+var id= (prompt("Document type: "));
+var number= (prompt("Identification number: "));
+
+console.log("Hi, my mombre is: "+name); 
+console.log("Document type: "+id); 
+console.log("Identification number: "+number); 
+
