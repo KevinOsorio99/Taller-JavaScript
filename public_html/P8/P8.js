58fba6cbf1a31a7fb09865aa05c6de47d60f83e2
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-07-22 20:34:53 -0500

    Terminado

---
+++ public_html/P8/P8.js
@@ -0,0 +1,17 @@
+var a=Number (prompt("Enter your first full number: "));
+var b=Number (prompt("Enter your second integer: "));
+
+var resultado=Number(a+b);
+console.log("The sum is: "+ resultado);
+
+var resultado=(a-b);
+console.log("The subtraction is: "+resultado);
+
+var resultado=(a*b);
+console.log("Multiplication is: "+resultado);
+
+var resultado=(a/b);
+console.log("The division is: "+resultado);
+
+
+
