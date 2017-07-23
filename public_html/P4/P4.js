f18cb0b73428f116b4a4a51cafb90f80b608ca1b
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-07-22 20:34:21 -0500

    Terminado

---
+++ public_html/P4/P4.js
@@ -0,0 +1,19 @@
+var name=(prompt("Enter your full name:"));
+console.log("Hello my Name Is: "+name);
+
+var id=(prompt("Document type:"));
+var number=(prompt("Write document number: "));
+console.log("My number of "+ id + " is "+ number);
+
+var gender=(prompt("Biological gender: "));
+console.log("Biological gender: "+ gender);
+
+var height=(prompt("Write your height: "));
+console.log("My height is: "+ height + " Mts ");
+
+var live=(prompt("¿Is it true that lives in Carthage? "));
+if (live === " yes "){
+    console.log("true");
+} else {
+    console.log("false");
+}
