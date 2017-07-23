6ab4e6e9f0839dbe17377eaaebc2a88210119076
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-07-22 20:36:32 -0500

    Terminado

---
+++ public_html/P14/P14.js
@@ -0,0 +1,18 @@
+//punto 1
+var a=(prompt("Enter number of employees: "));
+var result= 25832000/ a;
+console.log("Money received per employee: " + result);
+
+//punto 2
+var b=(prompt("Name of electrician:"));
+var c=Number= 45000 * 8;
+var d=Number= 20000;
+var result= (c-d);
+console.log("The electrician gain is:" +result);
+
+//punto 3
+var e=(2.54);
+var f=(12);
+var g=(5.280);
+var result=(e*f*g);
+console.log("Within a mile there are "+ result + " centimeter");
