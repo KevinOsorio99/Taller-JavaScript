f264ea387383416fda59199b0d91ed83558d284d
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-07-22 20:34:36 -0500

    Terminado

---
+++ public_html/P6/P6.js
@@ -0,0 +1,26 @@
+var a=Number("50");
+var b=Number("10");
+
+var result=(a^b);
+console.log("50 to 10 is: "+ result);
+
+var result=(b^a);
+console.log("10 to 50 is: "+ result);
+
+var result=(a*b);
+console.log("Multiplication between 50 and 10 is: "+ result);
+
+var result=(a/b);
+console.log("The division between 50 and 10 is: "+ result);
+
+var result=(b/a);
+console.log("The division between 10 and 50 is: "+ result);
+
+var result=(a%b);
+console.log("The division module between 50 and 10 is: "+ result);
+
+var result=(a-b);
+console.log("The subtraction between 50 and 10 is: "+ result);
+
+var result=(a+b);
+console.log("The sum between 50 and 10 is: "+ result);
