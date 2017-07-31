3c296e91c0f5df29c84ed05dd997e361234a765a
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-07-31 17:03:09 -0500

    terminado

---
+++ public_html/P16/P16.js
@@ -0,0 +1,8 @@
+var base= Number(prompt("Enter the base of the rectangle:"));
+var height= Number(prompt("Enter the height of the rectangle:"));
+
+var perimeter= base + base + height + height;
+console.log("The perimeter of the rectangle is: " + perimeter);
+
+var area= base * height;
+console.log("The area of the rectangle is: " + area);
