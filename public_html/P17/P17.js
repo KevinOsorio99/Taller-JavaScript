80ea597546cef54f5c518c4d64d4fd0dd8b77eb4
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-07-31 17:03:17 -0500

    terminado

---
+++ public_html/P17/P17.js
@@ -0,0 +1,5 @@
+var a=Number(prompt("Enter a number:"));
+var b=Number(prompt("Enter another number:"));
+var c=Number(prompt("Enter another number:"));
+var rule= (b*c) / a;
+console.log("The result is: " + rule);
