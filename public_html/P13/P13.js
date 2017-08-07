23da2a6ae41040d5f79fd6e2779b284d51dc759e
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-08-06 22:17:54 -0500

    terminado

--- public_html/P13/P13.js
+++ public_html/P13/P13.js
@@ -1,7 +1,28 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+var espacio="    ";
+var aste="*";
+var z="";
+var x="";
+var s=" ";
+for (i=1; i<=13; i++){
+    if (i===1){
+        console.log(z=espacio+aste);
+    }
+    if(i>=2 && i<=4){
+        if(i===3){
+            x=z;
+        }
+        console.log(z= z.slice(1) + aste + aste);
+    }
+    if(i===5){
+        console.log(x);
+    }
+    if(i===6){
+        console.log(x);
+    }
+    if(i>=7 && i<=9){
+        console.log( x = x.slice(1) + aste + aste);
+    }
+    if(i>9){
+        console.log(x= s + x.slice(0,-2));
+    }
+    }
