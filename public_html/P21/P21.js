51eaad4ab9eab6f7621aefabf1b16f2f5fb6bf9f
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-08-06 22:14:38 -0500

    terminado

---
+++ public_html/P21/P21.js
@@ -0,0 +1,7 @@
+var a=Number(prompt("Enter the side a:"));
+var b=Number(prompt("Enter the side b:"));
+
+var c = (a**2 + b**2);
+var c1= (c**(1/2));
+
+console.log("The hypotenuse would be: " + c**c1);
