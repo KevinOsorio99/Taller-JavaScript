d358e2d3e41cad61d2a60fc7891f6956814c06ed
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-08-31 13:47:01 -0500

    Terminado

---
+++ public_html/P31/P31.js
@@ -0,0 +1,95 @@
+class number{
+  constructor(){
+    this._array;
+    this._pair;
+    this._prime=[];
+    this._sumpair;
+    this._sumprime;
+    this._prompair;
+    this._promprime;
+  }
+  set array(array){
+    this._array=array;
+  }
+  get array(){
+    return this._array;
+  }
+  set pair(array){
+    this._pair=array.filter((e)=>{
+      return e%2===0;
+    });
+  }
+  get pair(){
+    return this._pair;
+  }
+  set prime(array){
+    array.forEach((e)=>{
+      var x=0;
+      for(let j=1; j<=e; j++){
+        if(e%j===0){
+          x++;
+        }
+      }
+      if(x<=2){
+        this._prime.push(e);
+      }
+    });
+  }
+  get prime(){
+    return this._prime;
+  }
+  set sumpair(pair){
+    var x=0;
+    this._pair.forEach((e)=>{
+      x=x+e;
+    });
+    this._sumpair=x;
+  }
+  get sumpair(){
+    return this._sumpair;
+  }
+  set sumprime(prime){
+    var x=0;
+    this._prime.forEach((e)=>{
+      x=x+e;
+    });
+    this._sumprime=x;
+  }
+  get sumprime(){
+    return this._sumprime;
+  }
+  set prompair(pair){
+    this._prompair=this._sumpair/this._pair.length;
+  }
+  get prompair(){
+    return this._prompair;
+  }
+  set promprime(prime){
+    this._promprime=this._sumprime/this._prime.length;
+  }
+  get promprime(){
+    return this._promprime;
+  }
+}
+
+var array=[];
+let size=Number(prompt("Ingrese el tamaño del arreglo:"));
+for (let i=0; i<size; i++){
+  array[i]=Number(prompt("Ingrese un numero:"));
+}
+
+var numbers= new number();
+numbers.array=array;
+console.log(numbers.array);
+numbers.pair=numbers.array;
+console.log("Los numeros pares son: " + numbers.pair);
+numbers.prime=numbers.array;
+console.log("Los numeros primos son: " + numbers.prime);
+numbers.sumpair=numbers.pair;
+console.log("La suma de los numeros pares es: " + numbers.sumpair);
+numbers.sumprime=numbers.prime;
+console.log("La suma de los numeros primos es: " + numbers.sumprime);
+numbers.prompair=numbers.pair;
+console.log("El promedio de los numeros pares es: " + numbers.prompair);
+numbers.promprime=numbers.prime;
+console.log("El promedio de los numeros primos es: " + numbers.promprime);
