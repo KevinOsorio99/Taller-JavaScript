9b6f182babea5a347a18cdea24a6aa968cd11b14
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-07 17:32:40 -0500

    Terminado

--- public_html/P41/P41.js
+++ public_html/P41/P41.js
@@ -2,6 +2,6 @@ function salve(){
   var a = $("#name").val();
   var b = $("#id").val();
   var c = $("#num").val();
-  var print = ("Hola, mi nombre es " + a + ", tipo de documento " + b + " mi numero de documento es " + c);
+  var print = ("Hello my Name Is " + a + ", document type " + b + " my document number is " + c);
   $("#text41").text(print);
 }
