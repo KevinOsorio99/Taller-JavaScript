deccae5da01a3c912f483b2939e3efcda26488a3
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-07 16:51:52 -0500

    Terminado

---
+++ public_html/P42/P42.js
@@ -0,0 +1,55 @@
+function a(){
+ var a = 50;
+ var b = 10;
+ var print = (a**b);
+ $("#result").val(print);
+} 
+
+function b(){
+  var a = 50;
+  var b = 10;
+  var print = (b**a);
+  $("#re").val(print);
+}
+
+function c(){
+  var a = 50;
+  var b = 10;
+  var print = (a*b);
+  $("#resu").val(print);
+}
+
+function d(){
+  var a = 50;
+  var b = 10;
+  var print = (a/b);
+  $("#resul").val(print);
+}
+
+function e(){
+  var a = 50;
+  var b = 10;
+  var print = (b/a);
+  $("#resulta").val(print);
+}
+
+function f(){
+  var a = 50;
+  var b = 10;
+  var print = (a%b);
+  $("#resultad").val(print);
+} 
+
+function g(){
+  var a = 50;
+  var b = 10;
+  var print= (a-b);
+  $("#resultado").val(print);
+}
+
+function h(){
+  var a = 50;
+  var b = 10;
+  var print= (a+b);
+  $("#resultados").val(print);
+}
