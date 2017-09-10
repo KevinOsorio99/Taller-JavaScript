f9ad2a67c8241f834db770e2a3d99327375748bd
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-09 21:04:33 -0500

    Terminado

--- public_html/P55/P55.js
+++ public_html/P55/P55.js
@@ -2,7 +2,7 @@ function salve(){
   var a = $("#sa").val();
   
   if (a > 0){
-    $("#text55").text("Introduzca un número inferior a cero");
+    $("#text55").text("Enter a number less than zero");
   }
   if (a < 0){
     a = a * (-1);
