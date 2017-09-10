d8b0153bdc7d751ba453db6596453c04941bc274
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-09 21:02:25 -0500

    Terminado

--- public_html/P44/P44.js
+++ public_html/P44/P44.js
@@ -3,10 +3,10 @@ var a = Number($("#s").val());
 var b = Number($("#d").val());
 
   $('#ko').modal();
-  var su = ("La suma de los dos numeros es: " + (a + b));
-  var re = ("La resta de los dos numeros es: " + (a - b));
-  var mul = ("La multiplicacion de los dos numeros es: " + (a * b));
-  var div = ("La division de los dos numeros es: " + (a / b));
+  var su = ("The sum of the two numbers is: " + (a + b));
+  var re = ("The subtraction of the two numbers is: " + (a - b));
+  var mul = ("The multiplication of the two numbers is: " + (a * b));
+  var div = ("The division of the two numbers is: " + (a / b));
   $("#rest").html(su + "<br>" + re + "<br>" + mul + "<br>" + div); 
 });
 
