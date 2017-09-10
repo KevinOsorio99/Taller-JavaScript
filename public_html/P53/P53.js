f65ea56878ee3c84b0ee18f87996c7825e642a32
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-09 21:04:12 -0500

    Terminado

--- public_html/P53/P53.js
+++ public_html/P53/P53.js
@@ -2,5 +2,5 @@ function salve(){
   var a = $("#nun1").val();
   var b = $("#nun2").val();
   var c = $("#nun3").val();
-  $("#text53").text("El resultado es: " + (b*c)/a);
+  $("#text53").text("The result is: " + (b*c)/a);
 }
