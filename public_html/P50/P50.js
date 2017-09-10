145a3d2f9feda099c59f92e104e5774efbb3ebb7
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-09 21:03:47 -0500

    Terminado

--- public_html/P50/P50.js
+++ public_html/P50/P50.js
@@ -2,7 +2,7 @@
 function salve(){
   var a = $("#emple").val();
   var result = (Math.round(25832000 / a));
-  $("#p1").text("Dinero recibido por empleado: " + (result));
+  $("#p1").text("Money received per employee: " + (result));
 } 
 
 //punto 2
@@ -11,11 +11,11 @@ function vol(){
   var b = $("#nu").val();
   var c = 45000 * b;
   var d = 20000;
-  $("#p2").text("El electricista " + a + " Gana " + (c-d));
+  $("#p2").text("The electrician " + a + " wins " + (c-d));
   
 //punto 3
 var e=(2.54);
 var f=(12);
 var g=(5.280);
-$("#p3").text("Dentro de una milla hay " + (Math.round(e*f*g)) + " Centimetros");
+$("#p3").text("Within a mile there are " + (Math.round(e*f*g)) + " Centimeters");
 }
