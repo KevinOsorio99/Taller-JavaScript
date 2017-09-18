37550a9f5d30fb75ae4217d69bda4005890a9c87
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-18 09:23:19 -0500

    Terminado

--- public_html/P40/P40.js
+++ public_html/P40/P40.js
@@ -1,7 +1,7 @@
 function salve(){
   var a = $("#name").val();
   var b = $("#id").val();
-  var c = $("#num").val();
+  var c = Number($("#number").val());
   var d = $("#gender").val();
   var e = $("#live").val();
   var print =("Hello my Name Is " + a + ", my type of document is " + b + ", document number is " + c + ", i m of gender " + d + " I live in Carthage " + e);  
