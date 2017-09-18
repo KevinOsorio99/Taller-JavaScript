e2fb6274c74c73f9cea27da690631e993fc86f80
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-18 09:21:17 -0500

    Terminado

--- public_html/P20/P20.js
+++ public_html/P20/P20.js
@@ -1,19 +1,27 @@
-var salaryMin=(prompt("Enter the minimum wage:"));
-var salary=Number(prompt("Enter your monthly salary:"));
-var days=Number(prompt("Enter the days worked:"));
-
-while (days > 30){
-    days=Number(prompt("Enter a number less than 30:"));
-}
-
-var salaryD = salary / 30;
-var salaryT = salaryD * days;
-if (salaryT <= (salaryMin * 2)) {
-    var help=Number(prompt("Enter Transport Assistance:"));
-    var salaryF = salaryT + help;
-    console.log(salaryF);
-} else if (salaryT >= (salaryT * 4)); {
-    var retention=Number(Prompt("Enter the winery:"));
-    salaryF= salaryT - retention;
-    console.log(salaryF);
+var day = Number (prompt("Number of days worked:"));
+if (day <= 30){
+  var salaryM = (740.000);
+  var salary = Number (prompt("What is your salary:"));
+  var daily = Math.round(salary / 30);
+  if (salary <= (salaryM * 2)){
+    var per = Number (prompt("how much is the percentage of transportation:"));
+    var per1 = (per / 100);
+    var persalary = Math.round(salary * per1);
+    var salary2 = Math.round(persalary + salary);
+    console.log("The salary is: " + salary);
+    console.log("The transport aid is: " + persalary);
+    console.log("the total salary is: " + salary2);
+  }else{
+    if (salary >= (salaryM * 4)){
+      var pert = Number (prompt("How much is the retention percentage:"));
+      console.log("His salary is: " + salary);
+      var perete = (pert / 100);
+      var persalary = (salary * perete);
+      var salary = (salary - persalary);
+      console.log("Retention is: " + persalary);
+      console.log("The total salary is: " + salary2);
+    }
+  }
+}else {
+    alert("maximun 30 days");
 }
