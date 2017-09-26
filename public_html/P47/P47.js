513200b5c861e7854e2e1f2304abb3a92326c060
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-26 12:40:13 -0500

    Terminado

--- public_html/P47/P47.js
+++ public_html/P47/P47.js
@@ -1,2 +1,10 @@
-
-
+  var aste= "*";
+  var maximo= 6;
+  var x= 1;
+  var n= "";
+  do{
+    n = n + aste;
+    $("#iniciar").append(n, "<br>");
+    console.log(n);
+    x++;
+  } while (x<= maximo);
