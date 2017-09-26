ff315c14860f8440ecc8a070308c694a037233ad
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-26 12:40:18 -0500

    Terminado

--- public_html/P48/P48.js
+++ public_html/P48/P48.js
@@ -1,1 +1,11 @@
-
+var aste= "-----*";
+var maximo= 5;
+var x= 1;
+var z="*";
+while (x<= maximo){
+    if(x===1){
+    $("#iniciar").append(aste, "<br>");
+}
+$("#iniciar").append(aste=aste.slice(1) + z, "<br>");
+    x++;
+} 
