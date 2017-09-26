fa22591a86e6519e20b8459a5590e824cb5ac89b
Kevin Osorio <Kevin Osorio@192.168.1.16>
2017-09-26 12:40:25 -0500

    Terminado

--- public_html/P49/P49.js
+++ public_html/P49/P49.js
@@ -1,3 +1,28 @@
-
-
-
+var espacio="----";
+var aste="*";
+var z="";
+var x="";
+var s="-";
+for (i = 1; i <= 13; i++) {
+  if (i === 1) {
+    $("#iniciar").append(z = espacio + aste, "<br>");
+  }
+  if (i >= 2 && i <= 4) {
+    if (i === 3) {
+      x = z;
+    }
+    $("#iniciar").append(z = z.slice(1) + aste + aste, "<br>");
+  }
+  if (i === 5) {
+    $("#iniciar").append(x, "<br>");
+  }
+  if (i === 6) {
+    $("#iniciar").append(x, "<br>");
+  }
+  if (i >= 7 && i <= 9) {
+    $("#iniciar").append(x = x.slice(1) + aste + aste, "<br>");
+  }
+  if (i > 9) {
+    $("#iniciar").append(x = s + x.slice(0, -2), "<br>");
+  }
+}
